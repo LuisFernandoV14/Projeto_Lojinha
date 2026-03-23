1890c64c03142921466a65984a78dbeaac5f0d62
Otavio Augusto Souza Silva <otavioaugustoss990@gmail.com>
2026-03-23 14:01:16 -0300

    Pequenos ajustes no schema

--- loja_schema.sql
+++ loja_schema.sql
@@ -30,7 +30,6 @@ CREATE TABLE IF NOT EXISTS Pedido (
 	id_pedido INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     id_cliente INT UNSIGNED,
     dat_pedido DATE,
-    id_itens_pedido INT UNSIGNED,
     
     CONSTRAINT fk_pedido_cliente FOREIGN KEY (id_cliente) REFERENCES Cliente (id_cliente)
     ON UPDATE CASCADE ON DELETE NO ACTION
@@ -43,19 +42,18 @@ CREATE TABLE IF NOT EXISTS Itens_Pedido (
     num_quantidade INT UNSIGNED,
     
     CONSTRAINT fk_itensPedido_pedidoid FOREIGN KEY (id_pedido) REFERENCES Pedido (id_pedido)
-    ON UPDATE CASCADE ON DELETE CASCADE
-);
+    ON UPDATE CASCADE ON DELETE CASCADE,
 
-ALTER TABLE Pedido 
-ADD CONSTRAINT fk_pedido_itensPedido FOREIGN KEY (id_itens_pedido) REFERENCES Itens_Pedido (id_itens_pedido)
-ON UPDATE CASCADE ON DELETE RESTRICT;
+    CONSTRAINT fk_itensPedido_produto FOREIGN KEY (id_produto) REFERENCES Produto (id_produto)
+    ON UPDATE CASCADE ON DELETE RESTRICT
+);
 
 CREATE TABLE IF NOT EXISTS Pagamento (
 	id_pagamento INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     id_pedido INT UNSIGNED,
-    val_pagamento DATE,
-    dat_pagamento DATE,
+    val_pagamento DECIMAL(10, 2),
+    dat_pagamento DATE DEFAULT NULL,
     
     CONSTRAINT fk_pagamento_pedido FOREIGN KEY (id_pedido) REFERENCES Pedido (id_pedido)
     ON UPDATE CASCADE ON DELETE RESTRICT
-)
+);
