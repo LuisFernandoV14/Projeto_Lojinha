8274bdf444a8bdc3a9f7a978276b42d83c5b82b9
Otavio Augusto Souza Silva <otavioaugustoss990@gmail.com>
2026-03-23 14:57:38 -0300

    model completo

---
+++ src/br/edu/ucb/lojinha/model/Cliente.java
@@ -0,0 +1,59 @@
+package br.edu.ucb.lojinha.model;
+
+import java.time.LocalDate;
+
+public class Cliente {
+    private int idCliente;
+    private String nome;
+    private String cpf;
+    private LocalDate dataNascimento; 
+    private Endereco endereco;
+
+    public Cliente(int idCliente, String nome, String cpf, LocalDate dataNascimento, Endereco endereco) {
+        this.idCliente = idCliente;
+        this.nome = nome;
+        this.cpf = cpf;
+        this.dataNascimento = dataNascimento;
+        this.endereco = endereco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public int getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(int idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+}
